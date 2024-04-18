45c223b133e50f74a72b9be031a61d34ba7b3a74
Ashok <tejaramoju111@gmail.com>
2024-04-18 22:43:07 +0530

    admin dahboard added

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent {
+
+  title:any;
+  actor:any;
+  actress:any;
+  writer:any;
+  director:any;
+  plot:any;
+
 
 }
