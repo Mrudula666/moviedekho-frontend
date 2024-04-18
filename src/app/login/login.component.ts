864bbe0f5a7c4988e24bff959646f547724fcc5f
Ashok <tejaramoju111@gmail.com>
2024-04-18 21:40:34 +0530

    init

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from 'auth.service.ts';
+// import { AuthService } from 'auth.service.ts';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, FormsModule],
+  standalone:true
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  email:any;
+  password:any;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
@@ -20,14 +29,7 @@ export class LoginComponent {
   onSubmit() {
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
-      this.authService.login(email, password).subscribe(
-        successResponse => {
-          // Handle successful login
-        },
-        errorResponse => {
-          // Handle login error
-        }
-      );
+
     }
   }
 }
